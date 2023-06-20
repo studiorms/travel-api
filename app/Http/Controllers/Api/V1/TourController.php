44009fe0556a6886e49d754e5040c71d610e61e2
Robert Smith <roberts@studiorms.com>
2023-06-19 22:15:48 -0700

    Filter tours by price and date

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,14 +3,36 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TourListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, TourListRequest $request)
     {
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy, function ($query) use ($request) {
+                if ($request->sortBy != 'price' || !in_array($request->sortOrder, ['asc', 'desc'])) {
+                    return;
+                }
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
             ->orderBy('starting_date')
             ->paginate();
 
